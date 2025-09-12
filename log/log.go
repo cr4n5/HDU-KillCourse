970c53c90089057f92fe277f6137013bcbf1a571
crane <136036346+cr4n5@users.noreply.github.com>
2025-09-13 01:48:59 +0800

    fix: 清除文件日志ANSI颜色代码

--- log/log.go
+++ log/log.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"regexp"
 
 	"github.com/fatih/color"
 )
@@ -13,6 +14,9 @@ var (
 	infoLogger  *log.Logger
 	errorLogger *log.Logger
 	debugLogger *log.Logger
+
+	infoFileLogger  *log.Logger
+	errorFileLogger *log.Logger
 )
 
 var (
@@ -21,6 +25,22 @@ var (
 )
 
 var logDir = "log_files"
+
+var ansiColorRegex = regexp.MustCompile("\x1b\\[[0-9;]*m")
+
+// 清除ANSI颜色代码
+func removeAnsiColors(args []interface{}) []interface{} {
+	result := make([]interface{}, len(args))
+	for i, arg := range args {
+		switch v := arg.(type) {
+		case string:
+			result[i] = ansiColorRegex.ReplaceAllString(v, "")
+		default:
+			result[i] = v
+		}
+	}
+	return result
+}
 
 func init() {
 	// 创建日志目录
@@ -38,22 +58,34 @@ func init() {
 		log.Fatalf("无法打开debug.log文件: %v", err)
 	}
 
-	appMultiWriter := io.MultiWriter(appFile, os.Stdout)
-	debugMultiWriter := io.MultiWriter(debugFile)
+	// 控制台
+	infoLogger = log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lmicroseconds)
+	errorLogger = log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lmicroseconds)
 
-	infoLogger = log.New(appMultiWriter, "", log.Ldate|log.Ltime|log.Lmicroseconds)
-	errorLogger = log.New(appMultiWriter, "", log.Ldate|log.Ltime|log.Lmicroseconds)
+	// 文件
+	infoFileLogger = log.New(appFile, "", log.Ldate|log.Ltime|log.Lmicroseconds)
+	errorFileLogger = log.New(appFile, "", log.Ldate|log.Ltime|log.Lmicroseconds)
+
+	// debug
+	debugMultiWriter := io.MultiWriter(debugFile)
 	debugLogger = log.New(debugMultiWriter, "", log.Ldate|log.Ltime|log.Lmicroseconds)
 }
 
 func Info(v ...interface{}) {
 	infoLogger.Println(append([]interface{}{InfoColor("[INFO]")}, v...)...)
+
+	cleanedArgs := removeAnsiColors(v)
+	infoFileLogger.Println(append([]interface{}{"[INFO]"}, cleanedArgs...)...)
 }
 
 func Error(v ...interface{}) {
 	errorLogger.Println(append([]interface{}{ErrorColor("[ERROR]")}, v...)...)
+
+	cleanedArgs := removeAnsiColors(v)
+	errorFileLogger.Println(append([]interface{}{"[ERROR]"}, cleanedArgs...)...)
 }
 
 func Debug(v ...interface{}) {
-	debugLogger.Println(append([]interface{}{"[DEBUG]"}, v...)...)
+	cleanedArgs := removeAnsiColors(v)
+	debugLogger.Println(append([]interface{}{"[DEBUG]"}, cleanedArgs...)...)
 }
